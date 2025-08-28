a3b9b71db7fe2cb77714bd1307ddbc09f8a80cc3
muhammad-taha-786 <tsyed6804@gmail.com>
2025-08-28 21:37:01 +0500

    Learning L9

--- Learning/Python/L8/index.py
+++ Learning/Python/L8/index.py
@@ -1,16 +1,23 @@
 # FUNCTIONS : 
-num1 = int(input("Enter your first number"))
-num2 = int(input("Enter your second number"))
-operators = input("Select operators : +, -, *, /")
-def add():
-    if operators == "+":
-        print(num1 + num2)
-    elif operators == "-":
-        print(num1 - num2)
-    elif operators == "*":
-        print(num1 * num2)
-    elif operators == "/":
-        print(num1 / num2)
-    else: 
-        print("Sorry")
-print(add())
+# num1 = int(input("Enter your first number"))
+# num2 = int(input("Enter your second number"))
+# operators = input("Select operators : +, -, *, /")
+# def add():
+#     if operators == "+":
+#         print(num1 + num2)
+#     elif operators == "-":
+#         print(num1 - num2)
+#     elif operators == "*":
+#         print(num1 * num2)
+#     elif operators == "/":
+#         print(num1 / num2)
+#     else: 
+#         print("Sorry")
+# print(add())
+
+# FUNCTION ARGUMENTS :-
+
+def add(a, b):
+    print("The sum of number is : ", (a+b))
+
+print(add(5,7))
---
+++ Learning/Python/L9/index.py
@@ -0,0 +1,14 @@
+# Introduction to Lists in Python
+
+# lst1 = [0,3,5,6,7,3,1]
+# lst2 = ["Taha", "Owais", "Saim"]
+
+# print(lst1)
+# print(lst2)
+
+lst1 = [0,3,5,6,3,1]
+
+if 7 in lst1:
+    print("Yes 7 in list ")
+else:
+    print("NO")
